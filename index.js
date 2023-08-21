d64a31fbb700b4b9b43ddbf936cd163560bbfcaa
xhifzanx <88442980+xhifzanx@users.noreply.github.com>
2023-08-21 12:08:49 +0000

    worked on second project of freecodecamp of node

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function(req, res) {
+  var ip_address = req.headers['x-forwarded-for']
+  var language = req.headers['accept-language']
+  var software = req.headers['user-agent']
+  
+  
+  res.json({ ipaddress: ip_address, language: language, software: software })
+})
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
